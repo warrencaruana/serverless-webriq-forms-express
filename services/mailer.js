a171f59d211259dd00d2367f7b7640935835098d
Dorell James <galangdj@gmail.com>
2020-06-10 16:50:04 +0800

    Configure SES mail

--- services/mailer.js
+++ services/mailer.js
@@ -5,6 +5,12 @@ const IS_OFFLINE = process.env.IS_OFFLINE;
 let transporter;
 
 if (IS_OFFLINE === "true") {
+  AWS.config.update({
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.ACCESS_SECRET_KEY,
+    region: process.env.APP_REGION || "us-west-2",
+  });
+
   transporter = nodemailer.createTransport({
     SES: new AWS.SES({
       apiVersion: "2010-12-01",
