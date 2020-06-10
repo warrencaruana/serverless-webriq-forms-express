ac867eb590316c8064fabc32bb64b53d6ce096dc
Dorell James <galangdj@gmail.com>
2020-06-10 15:15:11 +0800

    Use AWS SES for mailer

--- services/mailer.js
+++ services/mailer.js
@@ -5,20 +5,19 @@ const IS_OFFLINE = process.env.IS_OFFLINE;
 let transporter;
 
 if (IS_OFFLINE === "true") {
-  // transporter = nodemailer.createTransport({
-  //   SES: new AWS.SES({
-  //     apiVersion: "2010-12-01",
-  //   }),
-  //   sendingRate: 1, // max 1 messages/second
-  // });
   transporter = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-      user: process.env.MAILTRAP_USER,
-      pass: process.env.MAILTRAP_PASSWORD,
-    },
+    SES: new AWS.SES({
+      apiVersion: "2010-12-01",
+    }),
   });
+  // transporter = nodemailer.createTransport({
+  //   host: "smtp.mailtrap.io",
+  //   port: 2525,
+  //   auth: {
+  //     user: process.env.MAILTRAP_USER,
+  //     pass: process.env.MAILTRAP_PASSWORD,
+  //   },
+  // });
 } else {
   transporter = nodemailer.createTransport({
     service: "Mailgun",
